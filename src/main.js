9fb38a0291b290131449a56736424a6fb859c36d
Zakhse <zakhse@yandex.ru>
2017-12-22 23:01:36 +0300

    Add Yandex Metrika

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueYandexMetrika from 'vue-yandex-metrika';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css';
@@ -10,6 +11,11 @@ import router from './router';
 document.documentElement.style.setProperty('--main-color', `#${Math.random().toString(16).substr(-6)}`);
 
 Vue.use(ElementUI);
+Vue.use(VueYandexMetrika, {
+    id: 47116071,
+    router,
+    env: process.env.NODE_ENV
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
